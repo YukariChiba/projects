4c312aa6ed7327ca2a4fbda284a18f348fe9d891
Yukari Chiba <i@0x7f.cc>
2026-02-02 17:23:00 +0800

    adjust layout, update projects

--- src/App.vue
+++ src/App.vue
@@ -6,28 +6,165 @@
         subtitle="Yukari 的项目列表"
         prepend-avatar="/favicon.jpg"
         position="sticky"
+        class="mb-4"
         style="overflow-y: scroll; position: sticky; top: 0px; z-index: 114514"
-      />
-      <v-row class="ma-4">
-        <v-col
-          v-for="i in projects"
-          :key="i.name"
-          cols="12"
-          sm="6"
-          md="6"
-          lg="4"
-          class="d-flex flex-column"
-        >
-          <ProjectCard :i="i" />
-        </v-col>
+      >
+        <template #append>
+          <div class="d-flex align-center flex-wrap">
+            <v-select
+              v-model="groupMode"
+              :items="groupOptions"
+              label="分组方式"
+              density="compact"
+              variant="outlined"
+              hide-details
+              class="mr-4"
+              style="min-width: 150px"
+            />
+            <v-switch
+              v-model="showHidden"
+              label="显示无效项目"
+              color="error"
+              density="compact"
+              hide-details
+              inset
+            />
+          </div>
+        </template>
+      </v-card>
+
+      <template v-for="group in groupedProjects" :key="group.title">
+        <v-row v-if="groupMode !== 'none'" class="ma-4">
+          <v-col cols="12">
+            <div class="d-flex align-center">
+              <v-icon class="mr-2" size="large">{{
+                group.icon || "mdi-folder"
+              }}</v-icon>
+              <h2 class="text-h5 font-weight-bold">{{ group.title }}</h2>
+              <v-chip size="small" class="ml-2" variant="tonal">{{
+                group.list.length
+              }}</v-chip>
+            </div>
+            <v-divider class="mt-2" />
+          </v-col>
+        </v-row>
+
+        <v-row class="ma-4">
+          <v-col
+            v-for="project in group.list"
+            :key="project.name + group.title"
+            cols="12"
+            sm="6"
+            md="6"
+            lg="4"
+            class="d-flex flex-column"
+          >
+            <ProjectCard :i="project" />
+          </v-col>
+        </v-row>
+      </template>
+
+      <v-row v-if="groupedProjects.length === 0" class="ma-4">
+        <v-col class="text-center text-grey"> 没有找到符合条件的项目 </v-col>
       </v-row>
     </v-container>
   </v-app>
 </template>
 
 <script setup>
+import { ref, computed } from "vue";
 import projects_json from "@/assets/projects.json";
 import ProjectCard from "@/components/ProjectCard.vue";
 
-const projects = projects_json.projects;
+const rawProjects = projects_json.projects;
+const meta = projects_json.meta;
+const statusDef = projects_json.status;
+
+const groupMode = ref("none"); // none, lang, topic, status, year
+const showHidden = ref(false);
+
+const groupOptions = [
+  { title: "默认排序 (时间)", value: "none" },
+  { title: "编程语言", value: "lang" },
+  { title: "主题", value: "topic" },
+  { title: "开始年份", value: "year" },
+  { title: "维护状态", value: "status" },
+];
+
+const groupedProjects = computed(() => {
+  let list = rawProjects.filter(
+    (p) =>
+      showHidden.value ||
+      (p.status !== "deprecated" &&
+        p.status !== "abandoned" &&
+        p.status !== "not-maintaining"),
+  );
+
+  const sortByDate = (a, b) => {
+    const da = new Date(a.start.replace(".", "-"));
+    const db = new Date(b.start.replace(".", "-"));
+    return db - da;
+  };
+
+  list.sort(sortByDate);
+
+  if (groupMode.value === "none") {
+    return [{ title: "All", list: list, icon: "mdi-apps" }];
+  }
+
+  const groups = {};
+
+  if (groupMode.value === "year") {
+    list.forEach((p) => {
+      const year = p.start.substring(0, 4);
+      if (!groups[year]) groups[year] = [];
+      groups[year].push(p);
+    });
+    return Object.keys(groups)
+      .sort((a, b) => b - a)
+      .map((year) => ({
+        title: year,
+        list: groups[year],
+        icon: "mdi-calendar-clock",
+      }));
+  }
+
+  if (groupMode.value === "status") {
+    list.forEach((p) => {
+      const s = p.status;
+      if (!groups[s]) groups[s] = [];
+      groups[s].push(p);
+    });
+    return Object.keys(groups).map((s) => ({
+      title: statusDef[s]?.name || s,
+      list: groups[s],
+      icon: "mdi-list-status",
+    }));
+  }
+
+  if (groupMode.value === "lang" || groupMode.value === "topic") {
+    list.forEach((p) => {
+      const keys = p[groupMode.value];
+      if (!keys || keys.length === 0) {
+        if (!groups["Other"]) groups["Other"] = [];
+        groups["Other"].push(p);
+      } else {
+        keys.forEach((k) => {
+          if (!groups[k]) groups[k] = [];
+          groups[k].push(p);
+        });
+      }
+    });
+
+    return Object.keys(groups)
+      .sort()
+      .map((k) => ({
+        title: meta[k]?.name || k,
+        list: groups[k],
+        icon: meta[k]?.icon ? `mdi-${meta[k].icon}` : "mdi-label",
+      }));
+  }
+
+  return [];
+});
 </script>
--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,73 +1,97 @@
 <template>
-  <v-card class="flex d-flex flex-column" hover>
+  <v-card class="flex d-flex flex-column h-100" hover>
     <v-card-text class="text-right pb-0" style="margin-bottom: -20px">
       <v-chip
-        small
+        size="small"
         label
         dark
         class="mr-auto"
-        :color="statuslist[i.status].color"
+        :color="statuslist[i.status]?.color || 'grey'"
       >
-        {{ statuslist[i.status].name }}
+        {{ statuslist[i.status]?.name || i.status }}
       </v-chip>
     </v-card-text>
     <v-card-text class="pt-0">
-      <h2 class="title mb-2" color="text--primary">
+      <h2 class="text-h6 mb-2 font-weight-bold">
         {{ i.name }}
       </h2>
-      <p>{{ i.desc }}</p>
+      <p class="text-body-2">{{ i.desc }}</p>
       <div class="mt-2">
-        <v-chip
-          v-for="tag in i.tags"
-          :key="tag"
-          :color="taglist[tag].color"
-          outlined
-          small
-          class="mr-2 mb-1"
-          :prepend-icon="`mdi-${taglist[tag].icon}`"
-        >
-          {{ taglist[tag].name }}
-        </v-chip>
+        <template v-for="tagKey in displayTags" :key="tagKey">
+          <v-chip
+            v-if="meta[tagKey]"
+            :color="meta[tagKey].color"
+            variant="outlined"
+            size="small"
+            class="mr-2 mb-1"
+            :prepend-icon="`mdi-${meta[tagKey].icon}`"
+          >
+            {{ meta[tagKey].name }}
+          </v-chip>
+          <v-chip
+            v-else
+            color="grey"
+            variant="outlined"
+            size="small"
+            class="mr-2 mb-1"
+            prepend-icon="mdi-label"
+          >
+            {{ tagKey }}
+          </v-chip>
+        </template>
       </div>
     </v-card-text>
     <v-spacer />
     <v-divider />
     <v-card-actions>
-      <v-tooltip bottom v-for="link in i.link" :key="link.url">
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn
-            icon
-            v-on="on"
-            v-bind="attrs"
-            @click.stop="redirect_url(link.url)"
-          >
-            <v-icon>mdi-{{ link.icon }}</v-icon>
-          </v-btn>
-        </template>
-        <span>{{ link.url.replace("https://", "") }}</span>
-      </v-tooltip>
-      <v-tooltip bottom v-if="i.link.length == 0">
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn icon v-on="on" v-bind="attrs">
-            <v-icon>mdi-null</v-icon>
+      <template v-for="link in i.link" :key="link.url">
+        <v-tooltip location="bottom">
+          <template #activator="{ props }">
+            <v-btn
+              icon
+              v-bind="props"
+              variant="text"
+              @click.stop="open_url(link.url)"
+            >
+              <v-icon>mdi-{{ link.icon }}</v-icon>
+            </v-btn>
+          </template>
+          <span>{{ link.url.replace("https://", "") }}</span>
+        </v-tooltip>
+      </template>
+
+      <v-tooltip location="bottom" v-if="!i.link || i.link.length == 0">
+        <template v-slot:activator="{ props }">
+          <v-btn icon v-bind="props" variant="text" disabled>
+            <v-icon>mdi-link-off</v-icon>
           </v-btn>
         </template>
         <span>未有链接</span>
       </v-tooltip>
       <v-spacer />
-      <v-chip :text="i.start" class="text--secondary" label />
+      <v-chip size="small" label class="text-secondary">
+        {{ i.start }}
+      </v-chip>
     </v-card-actions>
   </v-card>
 </template>
+
 <script setup>
+import { computed } from "vue";
 import projects from "@/assets/projects.json";
 
-defineProps(["i"]);
+const props = defineProps(["i"]);
 
-const taglist = projects.tags;
+const meta = projects.meta;
 const statuslist = projects.status;
 
-const redirect_url = (url) => {
-  window.location.href = url;
+const displayTags = computed(() => {
+  const langs = props.i.lang || [];
+  const topics = props.i.topic || [];
+  return [...langs, ...topics];
+});
+
+const open_url = (url) => {
+  window.open(url, "_blank");
 };
 </script>
